a72e3cbb6fc5ac698d070810eaf3e4e1087990b5
Jadley Felipe <104441986+Jadley@users.noreply.github.com>
2022-08-22 10:13:27 -0300

    Create Ex-4.5-String.js

---
+++ Ex-4.5-String.js
@@ -0,0 +1,18 @@
+//Estruture três códigos, os quais devem conter uma função de manipulação de string que obtenha resultado.
+
+
+//convertendo inteiro em string.
+const numero = 250
+const conversao = new String(numero)
+console.log(conversao)
+
+
+//transformando o texto todo em maiúsculo.
+const texto = 'alerta, perigo à frente!'
+console.log(texto.toUpperCase())
+
+
+//mostra a quantidade de caracteres da palavra digitada.
+var palavra = prompt('Digite uma palavra: ');
+console.log('quantidade de caracteres: ' + palavra.length)
+
