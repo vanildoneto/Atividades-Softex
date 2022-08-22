a704a9f9277f38dbab5d58bade7c2aec2fccaff6
Jadley Felipe <104441986+Jadley@users.noreply.github.com>
2022-08-22 09:03:07 -0300

    Create Ex-4.4-pacotes.java

---
+++ Ex-4.4-pacotes.java
@@ -0,0 +1,11 @@
+import Dados.Cadastro.Clientes;
+package dados.banco;
+
+
+public class conta {
+    public static void main(string[] args) throws Exception{
+        Cliente cliente1 = new Cliente();
+        Cliente cliente2 = new Cliente();
+        Cliente cliente3 = new Cliente();
+    }
+}
